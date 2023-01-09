f0bf0fded28a58a7bf12355129c5cf98b590efe4
xavperrin <xl.perrin@gmail.com>
2023-01-09 20:56:56 +0100

    remove 12 js

---
+++ 12-modules-exports.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.COURSE = exports.PAGE_SIZE = void 0;
+exports.PAGE_SIZE = 100;
+exports.COURSE = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamentals, build practical projects",
+    lessonsCount: 10,
+    author: {
+        firstname: "Vasco",
+        lastname: "Cavalheiro"
+    }
+};
