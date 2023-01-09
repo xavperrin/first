3060fb6bb53f1b10c67fc3a244fde1d160930799
xavperrin <xl.perrin@gmail.com>
2023-01-09 15:08:52 +0100

    type assertion syntax

---
+++ type-assertions.js
@@ -0,0 +1,3 @@
+var input = document.getElementById("input-field-address"); // type assertion
+var input2 = document.getElementById("input-field-name"); // bracket syntax
+input.value;
---
+++ type-assertions.ts
@@ -0,0 +1,5 @@
+const input=document.getElementById("input-field-address") as HTMLInputElement;// type assertion
+const input2=<HTMLInputElement> document.getElementById("input-field-name");// bracket syntax
+
+input.value;
+
